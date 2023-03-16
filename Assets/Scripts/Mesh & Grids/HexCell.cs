06766e800b1af0ba50b6d5f1a2a82ee5b6378eec
mbertolini29 <mbertolini29@users.noreply.github.com>
2023-03-16 14:33:24 -0300

    intentando cambiar de color los hex

--- Assets/Scripts/Mesh & Grids/HexCell.cs
+++ Assets/Scripts/Mesh & Grids/HexCell.cs
@@ -2,9 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//noreste, este, sureste, suroeste, oeste y noroeste
+public enum HexDirection
+{
+    NE, E, SE, SW, W, NW
+}
+
+public static class HexDirectionExtensions
+{
+    public static HexDirection opposite (this HexDirection direction)
+    {
+        return (int)direction < 3 ? (direction + 3) : (direction - 3);
+    }
+}
+
 public class HexCell : MonoBehaviour
 {
     public HexCoordinates coordinates;
 
     public Color color;
+
+    [SerializeField] HexCell[] neighbors;
+
+    //recupera al vecino de una celda, en un direccion
+    public HexCell GetNighbor (HexDirection direction)
+    {
+        return neighbors[(int)direction];
+    }
+
+    //establecer un vecino
+    public void SetNeighbor(HexDirection direction, HexCell cell)
+    {
+        neighbors[(int)direction] = cell;
+        //los vecinos son bidireccionales, por lo tanto configuras la direccion opuesta.
+        //cell.neighbors[(int)direction.Opposite()] = this;
+
+    }
+
+    
 }
